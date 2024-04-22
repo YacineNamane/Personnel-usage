33aac5fbe359878a0394131dc9ff6f1f16bdfe6b
yacine namane <yacine.nam@outlook.fr>
2024-04-22 17:07:01 +0200

    Mise en place du formulaire de contact structure et style de base dans un premier temps fonctionnel

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import NavPannel from "../components/Header";
+import LoginForm from "../components/Login";
 function SignIn() {
+  const handleSubmit = (formData) => {
+    console.log(formData);
+  };
   return (
     <div>
       <NavPannel />
+      <LoginForm onSubmit={handleSubmit} />
     </div>
   );
 }
