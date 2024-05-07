52f19d15bbef95bab8e28e31a807c1a4a88dbce7
yacine namane <yacine.nam@outlook.fr>
2024-05-07 18:04:48 +0200

    mise a jour du formulaaire de connexion , envois de requete http post avec axios

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import AnnaG from "../assets/images/annag.gif";
 
 const LoginForm = ({ onSubmit }) => {
@@ -6,16 +7,20 @@ const LoginForm = ({ onSubmit }) => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (!email || !password) {
       setError("Veuillez remplir tous les champs");
       return;
     }
-    onSubmit({ email, password });
-    setEmail("");
-    setPassword("");
-    setError("");
+    try {
+      const response = await axios.post("/login", { email, password }); // Envoyer la requête POST au backend
+      console.log(response.data); // Afficher la réponse du backend (le jeton JWT)
+      // Stocker le jeton JWT dans le stockage local ou les cookies
+    } catch (error) {
+      setError("Identifiants invalides");
+      console.error("Erreur lors de la connexion :", error);
+    }
   };
 
   return (
